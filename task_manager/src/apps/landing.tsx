667e553be846355e8af3d01951aa7e7a1e77b8ef
Jocelyn Collado-Kuri <jcolladokuri@microsoft.com>
2024-09-18 10:00:28 -0700

    boilerplate for onboarding tasks page

--- task_manager/src/apps/landing.tsx
+++ task_manager/src/apps/landing.tsx
@@ -1,5 +1,49 @@
-export const Landing = (): JSX.Element => {
-    return ( <div>
-        <p>Landing Page !!!!</p>
-      </div>)
-};
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import { DetailsList, IColumn, MarqueeSelection, Selection, Separator } from '@fluentui/react';
+import { FluentProvider, MessageBar, MessageBarBody } from '@fluentui/react-components';
+
+interface LandingProps {
+  tasks: string[];
+}
+const Landing = ({tasks}: LandingProps) => {
+  const [numSelectedItems, setNumSelectedItems] = useState<number>(0);
+  const selection = new Selection({
+    onSelectionChanged: (): void => setNumSelectedItems(selection.getSelectedCount())
+  });
+  const items = tasks.map((task, idx) => { return { key: idx, name: task, value: idx } });
+  console.log(items)
+  const columns: IColumn[] = [
+    {
+      key: 'column1',
+      name: 'Task',
+      fieldName: 'name',
+      minWidth: 100,
+      maxWidth: 200,
+      isResizable: false
+    }
+  ]
+  return (
+    <div>
+      <div className="Landing">
+        <Separator>Task Manager</Separator>
+        <div className='LandingTasks'>
+          <div>
+            <MarqueeSelection selection={selection}>
+              <DetailsList
+                items={items}
+                columns={columns}
+                selection={selection}
+                setKey='set'
+                selectionPreservedOnEmptyClick={true}
+              />
+            </MarqueeSelection>
+          </div>
+        </div>
+        Onboard to {numSelectedItems} tasks
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
--- task_manager/src/routes.tsx
+++ task_manager/src/routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { getTasks } from './utils';
 import Landing from './apps/landing';
 import WeatherApp from './apps/weather';
 
 const AppRoutes = () => (
   <Router>
     <Routes>
-      <Route path="/landing" element={<Landing />} /> 
+      <Route path="/home" element={<Landing tasks={getTasks()}/>} /> 
       <Route path="/weather" element={<WeatherApp />} /> 
+      <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
   </Router>
 );
