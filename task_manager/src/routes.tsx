e9738812f174fb36dce274577bb7363a3f997233
REDMOND\sugayam <sugayam@microsoft.com>
2024-09-17 17:18:58 -0700

    Add node_modules to gitignore
    Add weather app
    
    Add axios dependency

--- task_manager/src/routes.tsx
+++ task_manager/src/routes.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Landing } from './apps/landing'; // Correctly import the 'Landing' component
+import WeatherApp from './apps/weather';
+import { Landing } from './apps/landing';
 
 const AppRoutes = () => (
   <Router>
     <Routes>
       <Route path="/landing" element={<Landing />} /> 
+      <Route path="/weather" element={<WeatherApp />} /> 
     </Routes>
   </Router>
 );
