17c7aacef76eaba979ed45f06d6599d748a2bd4e
Jocelyn Collado-Kuri <jcolladokuri@microsoft.com>
2024-09-27 14:02:12 -0700

    test for filterevents function

---
+++ src/tests/data/events.ts
@@ -0,0 +1,20 @@
+export const testEvents = [
+    {
+        name: "Arts & Crafts at the park",
+        start_time: "2024-09-01T10:00:00",
+        end_time: "2024-09-01T12:00:00",
+        link: "https://www.example.com",
+    },
+    {
+        name: "Yoga in the park",
+        start_time: "2024-09-01T08:00:00",
+        end_time: "2024-09-01T09:00:00",
+        link: "https://www.example.com",
+    },
+    {
+        name: "Music in the park",
+        start_time: "2024-09-01T18:00:00",
+        end_time: "2024-09-01T20:00:00",
+        link: "https://www.example.com",
+    },
+];
--- src/tests/utils.test.tsx
+++ src/tests/utils.test.tsx
@@ -3,9 +3,10 @@ import flight from "./assets/flight.jpg";
 import news from "./assets/news.jpg";
 import todo from "./assets/todo.jpg";
 import events from "./assets/events.jpg";
-import { getComponentForId, getServices, getVenueDetails } from "../utils";
+import { filterEventsByTime, getComponentForId, getServices, getVenueDetails } from "../utils";
 import { Venue } from "types";
 import { testVenue1, testVenue2 } from "./data/venue";
+import { testEvents } from "./data/events";
 
 jest.mock('../apps/weather', () => jest.fn(() => 'WeatherApp'));
 jest.mock('../apps/flights', () => jest.fn(() => 'FlightsApp'));
@@ -84,5 +85,38 @@ describe("getVenueDetails()", () => {
 
         expect(getVenueDetails(testVenue1)).toEqual("Phone: +1 212-768-1560, Address: Times Square, New York, NY, USA");
         expect(getVenueDetails(testVenue2)).toEqual("Phone: +1 212-736-3100, Address: Empire State Building, New York, NY, USA");
+    });
+});
+
+describe("filterEventsByTime()", () => {
+    it("only returns morning events", () => {
+        const filteredEvents = filterEventsByTime(testEvents, "Morning");
+        expect(filteredEvents.length).toBe(2);
+        const filteredEventsNames = filteredEvents.map((event) => event.name);
+        expect(filteredEventsNames).toEqual(["Arts & Crafts at the park", "Yoga in the park"]);
+    });
+
+    it("only returns evening events", () => {
+        const filteredEvents = filterEventsByTime(testEvents, "Evening");
+        expect(filteredEvents.length).toBe(1);
+        const filteredEventsNames = filteredEvents.map((event) => event.name);
+        expect(filteredEventsNames).toEqual(["Music in the park"]);
+    });
+
+    it("returns empty for empty initial array of events", () => {
+        const filteredEventsMorning = filterEventsByTime([], "Morning");
+        expect(filteredEventsMorning.length).toBe(0);
+        expect(filteredEventsMorning).toEqual([]);
+
+        const filteredEventsEvening = filterEventsByTime([], "Evening");
+        expect(filteredEventsEvening.length).toBe(0);
+        expect(filteredEventsEvening).toEqual([]);
+    });
+
+    it("returns original events for non existent filters", () => {
+        const filteredEvents = filterEventsByTime(testEvents, "random-filter");
+        expect(filteredEvents.length).toBe(3);
+        const filteredEventsNames = filteredEvents.map((event) => event.name);
+        expect(filteredEventsNames).toEqual(["Arts & Crafts at the park", "Yoga in the park", "Music in the park"]);
     });
 });
